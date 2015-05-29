a98fd1735ed3d6e3b5a4d677b0b49bd137a6f886
Aladdin <alaeddine.messadi@plinga.com>
2015-05-29 11:17:12 +0200

    Find Cell

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,33 +17,39 @@ for (var i = 0; i < GRID_SIZE; i++) {
 	merging.push(row2);
 }
 
-
 function makeNew(row, col) {
-  var number = Math.random() < 0.9 ? 2 : 4;
-  var color = pikColor(number);
-  var textcolor = textColor(number);
+	var number = Math.random() < 0.9 ? 2 : 4;
+	var color = pikColor(number);
+	var textcolor = textColor(number);
 
-  return grid[row][col] = $('<div>')
-    .css({
-	  background: color,
-	  color: textColor,
-      top  : row * 100 + 'px',
-      left : col * 100 + 'px'
-    })
-    .text(number)
-    .addClass('box')
-    .appendTo($('#grid'));
-	//$('#grid').append($('#grid .box:first'));
+	return grid[row][col] = $('<div>').css({
+		background : color,
+		color : textColor,
+		top : row * 100 + 'px',
+		left : col * 100 + 'px'
+	}).text(number).addClass('box').appendTo($('#grid'));
+	// $('#grid').append($('#grid .box:first'));
 }
 
 function hasUnusedCell() {
-    for (var i = 0; i < GRID_SIZE; i++) {
-      for (var j = 0; j < GRID_SIZE; j++) {
-        if (grid[i][j] == null) {
-          return true;
-        }
-      }
-    }
+	for (var i = 0; i < GRID_SIZE; i++) {
+		for (var j = 0; j < GRID_SIZE; j++) {
+			if (grid[i][j] == null) {
+				return true;
+			}
+		}
+	}
 
-    return false;
-  }
+	return false;
+}
+
+function findUnusedCell() {
+	while (true) {
+		var row = Math.floor(Math.random() * GRID_SIZE);
+		var col = Math.floor(Math.random() * GRID_SIZE);
+
+		if (grid[row][col] == null) {
+			return [ row, col ];
+		}
+	}
+}
