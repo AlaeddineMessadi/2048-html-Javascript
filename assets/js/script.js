edb6a324414379a642684e3e062ea534e095ce17
Aladdin <alaeddine.messadi@plinga.com>
2015-05-29 11:11:22 +0200

    Create and Initialize rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,16 @@ var ANIMATION_DURATION = 100;
 
 var grid = [];
 var merging = [];
+
+for (var i = 0; i < GRID_SIZE; i++) {
+	var row1 = [];
+	var row2 = [];
+
+	for (var j = 0; j < GRID_SIZE; j++) {
+		row1.push(null);
+		row2.push(false);
+	}
+
+	grid.push(row1);
+	merging.push(row2);
+}
